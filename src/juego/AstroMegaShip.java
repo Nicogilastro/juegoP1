6c6fc58a44a69554121336128a9a3c4d4aadec70
Nicogilastro <75554628+Nicogilastro@users.noreply.github.com>
2023-05-04 22:51:53 -0300

    1

---
+++ src/juego/AstroMegaShip.java
@@ -0,0 +1,25 @@
+package juego;
+
+import java.awt.Image;
+
+public class AstroMegaShip {
+	private int vidas;
+	private Image imagen;
+	
+	public Image getImagen() {
+		return imagen;
+	}
+	
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
+	}
+	
+	public int getVidas() {
+		return vidas;
+	}
+	
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+	
+}
