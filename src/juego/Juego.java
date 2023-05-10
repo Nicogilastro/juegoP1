2429febe72b72e8e0fe97b00a2c887460225d733
Nicogilastro <75554628+Nicogilastro@users.noreply.github.com>
2023-05-10 20:34:36 -0300

    1

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -10,8 +10,7 @@ import entorno.Entorno;
 import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
-public class Juego extends InterfaceJuego
-{
+public class Juego extends InterfaceJuego {
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	Spaceship navecita;
@@ -20,129 +19,125 @@ public class Juego extends InterfaceJuego
 	Destructores[] destructoresArr;
 	Destructores destructor;
 	fondo fondo;
-	
+
 	// Variables y métodos propios de cada grupo
 	// ...
-	
-	Juego()
-	{
+
+	Juego() {
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Lost Galaxian - Grupo 3 - v1", 600, 1600);
-		
+
 		// Inicializar lo que haga falta para el juego
 		// ...
-		
-		navecita = new Spaceship(entorno.ancho()/2, entorno.alto() - 100, 3, 2);
-	
-		
-//		genera un numero random de asteroides		
-		
+
+		navecita = new Spaceship(entorno.ancho() / 2, entorno.alto() - 100, 3, 2);
+
+		// genera un numero random de asteroides
+
 		Random random = new Random();
 		int rand = 0;
-		while (true){
-		    rand = random.nextInt(4, 7);
-		    if(rand !=0) break;
+		while (true) {
+			rand = random.nextInt(4, 7);
+			if (rand != 0)
+				break;
 		}
-//		array de asteroides
+		// array de asteroides
 		asteroidesArr = new Asteroides[rand];
-		
-		for(int i=1; i <= rand; i++) {
-			asteroide = new Asteroides(random.nextInt(0, 800), random.nextInt(0, 200), 1, Math.PI/4, 30);
-			asteroidesArr[i-1] = asteroide;
-		}
-		
-//		genera un numero random de destructores	
-		
-		Random random2 = new Random();
-		int rand2 = 0;
-		while (true){
-		    rand2 = random2.nextInt(4, 7);
-		    if(rand !=0) break;
-		}
-//		array de destructores
-		destructoresArr = new Destructores[rand2];
-		
-		for(int i=1; i <= rand2; i++) {
-			destructor = new Destructores(random.nextInt(rand2*5, 800), random.nextInt(rand2*3, 300), 1);
-			destructor.fueraDePantalla(random.nextInt(rand2*5, 800), random.nextInt(rand2*3, 300));
-			destructoresArr[i-1] = destructor;
-			
+
+		for (int i = 1; i <= rand; i++) {
+			asteroide = new Asteroides(random.nextInt(0, 800), random.nextInt(0, 200), 1, Math.PI / 4, 30);
+			asteroidesArr[i - 1] = asteroide;
 		}
 
-		fondo = new fondo(entorno,this);
+		// genera un numero random de destructores
+
+		Random random2 = new Random();
+		int rand2 = 0;
+		while (true) {
+			rand2 = random2.nextInt(4, 7);
+			if (rand != 0)
+				break;
+		}
+		// array de destructores
+		destructoresArr = new Destructores[rand2];
+
+		for (int i = 1; i <= rand2; i++) {
+			destructor = new Destructores(random.nextInt(rand2 * 5, 800), random.nextInt(rand2 * 3, 300), 1);
+			destructor.fueraDePantalla(random.nextInt(rand2 * 5, 800), random.nextInt(rand2 * 3, 300));
+			destructoresArr[i - 1] = destructor;
+
+		}
+
+		fondo = new fondo(entorno, this);
 		// Inicia el juego!
 		this.entorno.iniciar();
-		
+
 	}
-	
+
 	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
+	 * Durante el juego, el método tick() será ejecutado en cada instante y
+	 * por lo tanto es el método más importante de esta clase. Aquí se debe
+	 * actualizar el estado interno del juego para simular el paso del tiempo
 	 * (ver el enunciado del TP para mayor detalle).
 	 */
-	public void tick()
-	{
+	public void tick() {
 		// Procesamiento de un instante de tiempo
 		// ...
-		
-//		Comportamiento de las teclas
-		
+
+		// Comportamiento de las teclas
+
 		if (entorno.estaPresionada(entorno.TECLA_ESPACIO))
 			navecita.disparar();
-		
+
 		if (entorno.estaPresionada(entorno.TECLA_DERECHA) || entorno.estaPresionada('d'))
 			navecita.moverDerecha(entorno);
 
 		if (entorno.estaPresionada(entorno.TECLA_IZQUIERDA) || entorno.estaPresionada('a'))
 			navecita.moverIzquierda(entorno);
-		
-//		dibujo la nave
+
+		// dibujo la nave
 		navecita.dibujarse(entorno);
-		
-//		dibujo cada asteroide
-		for(int i=0; i < asteroidesArr.length; i++) {
-			if(asteroidesArr[i] == null) {
+
+		// dibujo cada asteroide
+		for (int i = 0; i < asteroidesArr.length; i++) {
+			if (asteroidesArr[i] == null) {
 				continue;
 			} else {
 				asteroidesArr[i].dibujarse(entorno);
-				asteroidesArr[i].mover(i%2 == 0 ? 1 : -1);
-				
+				asteroidesArr[i].mover(i % 2 == 0 ? 1 : -1);
+
 			}
 		}
-		
-//		dibujo cada destructor
 
-		for(int i=1; i <= destructoresArr.length; i++) {
-			if(destructoresArr[i-1] == null) {
+		// dibujo cada destructor
+
+		for (int i = 1; i <= destructoresArr.length; i++) {
+			if (destructoresArr[i - 1] == null) {
 				continue;
 			} else {
-				destructoresArr[i-1].dibujarse(entorno);
-				
+				destructoresArr[i - 1].dibujarse(entorno);
+
 			}
 		}
 
-//		muevo los destructores de izq a der
+		// muevo los destructores de izq a der
 
-	    for(int i=0; i < destructoresArr.length; i++) {
-			if(destructoresArr[i] == null) {
+		for (int i = 0; i < destructoresArr.length; i++) {
+			if (destructoresArr[i] == null) {
 				continue;
 			} else {
-				if(i % 2 == 0)
+				if (i % 2 == 0)
 					destructoresArr[i].mover(1.5);
-				else 
+				else
 					destructoresArr[i].mover(1.5);
-				
+
 			}
 		}
 
-		
-// muevo los destructores		
-		
-		
-		
-//		dibujo lost textos
-//		vidas
+		// muevo los destructores
+
+		// dibujo lost textos
+		// vidas
 		entorno.cambiarFont("Arial", 18, Color.white);
 		entorno.escribirTexto("Vidas: " + navecita.getVidas(), 50, 50);
 
@@ -150,18 +145,16 @@ public class Juego extends InterfaceJuego
 		destructor.colision(navecita, destructoresArr);
 		destructor.superponen(destructoresArr, asteroidesArr);
 
-		
-//		estado de la navecita vidas, proyectiles etc
-		if(navecita.getVidas() == 0) {
+		// estado de la navecita vidas, proyectiles etc
+		if (navecita.getVidas() == 0) {
 			System.exit(0);
 		}
-//		dibujo el fondo
-		//fondo.dibujar(entorno);
-	}	
-	
+		// dibujo el fondo
+		// fondo.dibujar(entorno);
+	}
+
 	@SuppressWarnings("unused")
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Juego juego = new Juego();
 	}
 }
